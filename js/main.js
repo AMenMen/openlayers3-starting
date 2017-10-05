5e997a98ba0769dd2e91c66a0955739d005c33b2
AMenMen <tvvkiet@gmail.com>
2017-10-05 16:31:12 +0700

    Add new first files and directories
    This is a small project which in the progressing I am learning about Openlayers 3 framework

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// function add() {
+// 	var x = 12;
+// 	var y = 23;
+// 	var z = x + y;
+//    document.getElementById("add").innerHTML = z;
+// }
+// function myFunction(){
+// 	document.getElementById("demo").innerHTML = "My Function Was Called";
+// }
+
+// function myFirstJavaScript() {
+// 	document.getElementById("myFirstJavaScript").innerHTML = "M";
+// }
+
+// window.onload = function(){
+// 	var lon = 0;
+// 	var lat = 0;
+// 	var OSMSource = new ol.source.OSM();
+// 	// var source1 = new ol.source.MapQuest({layer: 'osm'});
+// 	var baseLayer = new ol.layer.Tile({
+// 		source: OSMSource
+// 	});
+// 	var view = new ol.View({
+// 		center: [lon,lat],
+// 		zoom: 2,
+// 		maxZoom: 18,
+// 		minZoom: 2
+// 	});
+// 	var map = new ol.Map({
+// 		target: 'map',
+// 		view: view,
+// 		layers:[baseLayer],
+// 		controls: ol.control.defaults().extend([
+// 			new ol.control.ScaleLine(),
+// 			new ol.control.ZoomSlider()
+// 		]),
+// 		renderer: 'canvas'
+// 	});
+
+
+
+	
+
+// };
